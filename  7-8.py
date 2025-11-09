bc8223dfa7f973f1a98870054517c554bd54ac6b
Kat5270 <kisshyunwoo@hotmail.com>
2025-11-10 06:18:59 +0800

    Add files via upload

---
+++  7-8.py
@@ -0,0 +1,14 @@
+sandwich_orders = ['roast beef', 'turkey', 'grilled cheese', 'veggie']
+finished_sandwiches = []
+
+while sandwich_orders:
+	current_sandwich = sandwich_orders.pop()
+	print(f"I'm working on your {current_sandwich} sandwich.")
+	finished_sandwiches.append(current_sandwich)
+
+for sandwich in finished_sandwiches:
+	print(f"I made your {sandwich} sandwichprint("\n")	.") 
+
+
+	
+ 
